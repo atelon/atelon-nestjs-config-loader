bc4865b3b741a2f5d1788a7ebfd73449882d3da6
atelon <valiasbest@gmail.com>
2021-06-28 21:19:10 +0300

    fix(package-lock.json): ignore package-lock.json

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
     }],
     "@semantic-release/npm",
     ["@semantic-release/git", {
-      "assets": ["CHANGELOG.md", "package.json", "package-lock.json", "npm-shrinkwrap.json"],
+      "assets": ["CHANGELOG.md", "package.json", "npm-shrinkwrap.json"],
     }],
     "@semantic-release/github",
   ]
